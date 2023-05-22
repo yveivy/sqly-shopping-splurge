2dc239ac665f18319ec84d845c03a3c0a8a4ff33
Valais Girl <Yevette@Hal-71.local>
2023-05-21 23:18:30 -0500

    import sequelize

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-
+const sequelize = require('./config/connection')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on:${PORT}`);
